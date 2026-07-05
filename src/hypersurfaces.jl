83c5887edf39a0b3f8a4d82ef73b9094b26c4e55
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2026-07-06 00:38:28 +0100

    Add missing row break

--- src/hypersurfaces.jl
+++ src/hypersurfaces.jl
@@ -13,7 +13,8 @@ sample_points
 A hypersurface $\mathcal{H}$ in $\mathbb{C}^k$ that arises through projection of $(k-1)$-dimensional variety in a higher-dimensional ambient space.
 
 The hypersurface $\mathcal{H}$ is represented by a [`PseudoWitnessSet`](@ref).
-"""struct ProjectedHypersurface{TC} <: HC.AbstractSystem
+"""
+struct ProjectedHypersurface{TC} <: HC.AbstractSystem
     PWS::PseudoWitnessSet
     projection_vars::Vector{HC.Variable}
     GC::TC
